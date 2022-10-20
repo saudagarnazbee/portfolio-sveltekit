a70c287dc2ce5fb63e8cef22b4aa1cdc9bd9009c
nazrin <mat8392@gmail.com>
2022-10-20 10:01:21 +0800

    Updates

---
+++ _app/immutable/chunks/0-5f532127.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_layout-244212a4.js";import{default as t}from"../components/pages/_layout.svelte-e326de1b.js";export{t as component,r as shared};
---
+++ _app/immutable/chunks/2-5b8d4ef9.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-57528461.js";export{t as component};
