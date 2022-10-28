c8bae9049fbfc04964319300ce8c47a8ae2e685a
nazrin <mat8392@gmail.com>
2022-10-28 12:55:20 +0800

    Updates

---
+++ _app/immutable/chunks/1-6f639d22.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-a4687ce3.js";export{t as component};
---
+++ _app/immutable/chunks/2-a3de142d.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-6ee558fe.js";export{t as component};
