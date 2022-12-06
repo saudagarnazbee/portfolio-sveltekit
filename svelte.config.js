380305b350b3abaaf0556205a56c6800b15a7628
nazrin <mat8392@gmail.com>
2022-12-06 11:05:25 +0800

    npm update

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import adapterStatic from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
+// cannot add sveltekit $app : https://stackoverflow.com/questions/70339952/sveltekit-app-cannot-be-found-in-svelte-config-js
 const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
